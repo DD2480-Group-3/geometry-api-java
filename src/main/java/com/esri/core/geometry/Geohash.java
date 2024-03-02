42b5287f84c7d8e640927d129b459957ab4f7a76
wenjj2000 <wenjj2000@gmail.com>
2024-03-02 23:14:06 +0100

    feat : Added toGeoHash function with 2 helper function BinaryToBase32 and converyTobinary

--- src/main/java/com/esri/core/geometry/Geohash.java
+++ src/main/java/com/esri/core/geometry/Geohash.java
@@ -6,94 +6,143 @@ import java.security.InvalidParameterException;
  * Helper class to work with geohash
  */
 public class Geohash {
-    
-    private static final String base32 = "0123456789bcdefghjkmnpqrstuvwxyz";
-    
-    private static final String INVALID_CHARACTER_MESSAGE = "Invalid character in geohash: ";
-    private static final String GEOHASH_EXCEED_MAX_PRECISION_MESSAGE = "Precision to high in geohash (max 24)";
-    
-    /**
-     * Create an evelope from a given geohash
-     * @param geoHash
-     * @return The envelope that corresponds to the geohash
-     * @throws InvalidParameterException if the precision of geoHash is greater than 24 characters
-     */
-    public static Envelope2D geohashToEnvelope(String geoHash){
-        if(geoHash.length() > 24){
-            throw new InvalidParameterException(GEOHASH_EXCEED_MAX_PRECISION_MESSAGE);
-        } 
 
-        long latBits = 0;
-        long lonBits = 0;
-        for(int i=0; i < geoHash.length(); i++){
-            int pos = base32.indexOf(geoHash.charAt(i));
-            if(pos == -1){
-                throw new InvalidParameterException(
-                    new StringBuilder(INVALID_CHARACTER_MESSAGE)
-                        .append('\'')
-                        .append(geoHash.charAt(i))
-                        .append('\'').toString());
-            } 
-            
-            if(i%2 == 0){
-                lonBits = (lonBits << 3) | ((pos>>2) & 4) | ((pos>>1) & 2) | (pos & 1); 
-                latBits = (latBits << 2) | ((pos>>2) & 2) | ((pos>>1) & 1);
-            }else{
-                latBits = (latBits << 3) | ((pos>>2) & 4) | ((pos>>1) & 2) | (pos & 1); 
-                lonBits = (lonBits << 2) | ((pos>>2) & 2) | ((pos>>1) & 1);
-            }
-        }
+  private static final String base32 = "0123456789bcdefghjkmnpqrstuvwxyz";
 
-        int lonBitsSize = (int) Math.ceil(geoHash.length()*5/2.0);
-        int latBitsSize = geoHash.length()*5 - lonBitsSize;
+  private static final String INVALID_CHARACTER_MESSAGE =
+    "Invalid character in geohash: ";
+  private static final String GEOHASH_EXCEED_MAX_PRECISION_MESSAGE =
+    "Precision to high in geohash (max 24)";
 
-        double lat = -90;
-        double latPrecision = 90;
-        for(int i = 0; i < latBitsSize; i++){
-            if(((1 << (latBitsSize-1-i)) & latBits) != 0){
-                lat += latPrecision;
-            }
-            latPrecision /= 2;
-        }
-
-        double lon = -180;
-        double lonPrecision = 180;
-        for(int i = 0; i < lonBitsSize; i++){
-            if(((1 << (lonBitsSize-1-i)) & lonBits) != 0){
-                lon += lonPrecision;
-            }
-            lonPrecision /= 2;
-        }
-
-        return new Envelope2D(lon, lat, lon + lonPrecision*2, lat + latPrecision*2);
+  /**
+   * Create an evelope from a given geohash
+   * @param geoHash
+   * @return The envelope that corresponds to the geohash
+   * @throws InvalidParameterException if the precision of geoHash is greater than 24 characters
+   */
+  public static Envelope2D geohashToEnvelope(String geoHash) {
+    if (geoHash.length() > 24) {
+      throw new InvalidParameterException(GEOHASH_EXCEED_MAX_PRECISION_MESSAGE);
     }
 
-    /**
-     * Computes the geohash that contains a point at a certain precision
-     * @param pt A point represented as lat/long pair
-     * @param characterLength - The precision of the geohash
-     * @return The geohash of containing pt as a String
-     */
-    public static String toGeohash(Point2D pt, int characterLength){
-        
-        return "";      
+    long latBits = 0;
+    long lonBits = 0;
+    for (int i = 0; i < geoHash.length(); i++) {
+      int pos = base32.indexOf(geoHash.charAt(i));
+      if (pos == -1) {
+        throw new InvalidParameterException(
+          new StringBuilder(INVALID_CHARACTER_MESSAGE)
+            .append('\'')
+            .append(geoHash.charAt(i))
+            .append('\'')
+            .toString()
+        );
+      }
+
+      if (i % 2 == 0) {
+        lonBits =
+          (lonBits << 3) | ((pos >> 2) & 4) | ((pos >> 1) & 2) | (pos & 1);
+        latBits = (latBits << 2) | ((pos >> 2) & 2) | ((pos >> 1) & 1);
+      } else {
+        latBits =
+          (latBits << 3) | ((pos >> 2) & 4) | ((pos >> 1) & 2) | (pos & 1);
+        lonBits = (lonBits << 2) | ((pos >> 2) & 2) | ((pos >> 1) & 1);
+      }
     }
 
-    /**
-     * Compute the longest geohash that contains the envelope
-     * @param envelope
-     * @return the geohash as a string
-     */
-    public static String containingGeohash(Envelope2D envelope){
-        return "";
+    int lonBitsSize = (int) Math.ceil(geoHash.length() * 5 / 2.0);
+    int latBitsSize = geoHash.length() * 5 - lonBitsSize;
+
+    double lat = -90;
+    double latPrecision = 90;
+    for (int i = 0; i < latBitsSize; i++) {
+      if (((1 << (latBitsSize - 1 - i)) & latBits) != 0) {
+        lat += latPrecision;
+      }
+      latPrecision /= 2;
     }
 
-    /**
-     * 
-     * @param envelope 
-     * @return up to four geohashes that completely cover given envelope
-     */
-    public static String[] coveringGeohash(Envelope2D envelope){
-        return new String[]{};
+    double lon = -180;
+    double lonPrecision = 180;
+    for (int i = 0; i < lonBitsSize; i++) {
+      if (((1 << (lonBitsSize - 1 - i)) & lonBits) != 0) {
+        lon += lonPrecision;
+      }
+      lonPrecision /= 2;
     }
+
+    return new Envelope2D(
+      lon,
+      lat,
+      lon + lonPrecision * 2,
+      lat + latPrecision * 2
+    );
+  }
+
+  /**
+   * Computes the geohash that contains a point at a certain precision
+   * @param pt A point represented as lat/long pair
+   * @param characterLength - The precision of the geohash
+   * @return The geohash of containing pt as a String
+   */
+  public static String toGeohash(Point2D pt, int characterLength) {
+    int precision = characterLength;
+    double[] latRange = new double[] { -90, 90 };
+    double[] lonRange = new double[] { -180, 180 };
+    double lat = pt.x;
+    double lon = pt.y;
+    String latBitStr = convertToBinary(lat, latRange, precision);
+    String lonBitStr = convertToBinary(lon, lonRange, precision);
+
+    StringBuilder interwovenBin = new StringBuilder();
+    for (int i = 0; i < precision; i++) {
+      interwovenBin.append(latBitStr.charAt(i));
+      interwovenBin.append(lonBitStr.charAt(i));
+    }
+
+    return binaryToBase32(interwovenBin.toString());
+  }
+
+  static String binaryToBase32(String binStr) {
+    StringBuilder base32Str = new StringBuilder();
+    for (int i = 0; i < binStr.length(); i += 5) {
+      String chunk = binStr.substring(i, i + 5);
+      int decVal = Integer.valueOf(chunk, 2);
+      base32Str.append(base32.charAt(decVal));
+    }
+    return base32Str.toString();
+  }
+
+  static String convertToBinary(double value, double[] range, int precision) {
+    StringBuilder binString = new StringBuilder();
+    for (int i = 0; i < precision; i++) {
+      double mid = (range[0] + range[1]) / 2;
+      if (value >= mid) {
+        binString.append("1");
+        range[0] = mid;
+      } else {
+        binString.append("0");
+        range[1] = mid;
+      }
+    }
+    return binString.toString();
+  }
+
+  /**
+   * Compute the longest geohash that contains the envelope
+   * @param envelope
+   * @return the geohash as a string
+   */
+  public static String containingGeohash(Envelope2D envelope) {
+    return "";
+  }
+
+  /**
+   *
+   * @param envelope
+   * @return up to four geohashes that completely cover given envelope
+   */
+  public static String[] coveringGeohash(Envelope2D envelope) {
+    return new String[] {};
+  }
 }
